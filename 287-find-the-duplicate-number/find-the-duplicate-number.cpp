c9c5f099840b7dc8aee5d82f94d4a1855d82e50c
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-16 00:10:37 +0500

    Update find-the-duplicate-number.cpp

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -17,4 +17,24 @@ public:
         }
         return slow;
     }
+    /*int binarySearch(vector<int>& nums){
+        int left = 1, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+            (count > mid) 
+                ? right = mid 
+                : left = mid + 1;
+            }
+            return left;
+        }
+    int findDuplicate(vector<int>& nums) {
+        int dup = binarySearch(nums);
+        return dup;
+    }*/ 
 };
