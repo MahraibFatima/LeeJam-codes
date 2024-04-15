9399d5997c42ed9822632c4fd44f19fdb2f8f7e7
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-15 23:57:00 +0500

    Time: 80 ms (59.18%) | Memory: 63.6 MB (75.73%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums){
+    /*int binarySearch(vector<int>& nums){
         int left = 1, right = nums.size() - 1;
 
         while (left < right) {
@@ -20,5 +20,22 @@ public:
     int findDuplicate(vector<int>& nums) {
         int dup = binarySearch(nums);
         return dup;
-    }      
+    }*/
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0], fast=nums[0];
+
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast){
+                break;
+            }
+        }   
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
 };
