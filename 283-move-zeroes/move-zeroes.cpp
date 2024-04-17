c8d7516377e8a4ed1681decc146a9f1156a07975
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-17 23:31:30 +0500

    Time: 14 ms (72.60%) | Memory: 21.5 MB (73.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-  int i = 0, j = 0;
-  while (j < nums.size()) {
-    if (nums[j] != 0) {
-      swap(nums[i], nums[j]);
-      i++;
+        int i = 0, j = 0;
+        while (j < nums.size()) {
+            if (nums[j] != 0) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        j++;
+        }
     }
-    j++;
-  }
-}
 };
