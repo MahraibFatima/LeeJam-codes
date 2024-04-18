5ed62ed469688c8e391874a85177d4dd8b60d26f
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-18 22:06:58 +0500

    Time: 7 ms (56.20%) | Memory: 7.1 MB (72.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,6 +3,10 @@ public:
     int fib(int n) {
         if (n==0 || n==1)
             return n;
+        if(n==2)
+            return 1;
+        else if(n == 3)
+            return 2;
         return fib(n-1) + fib(n-2);
     }
 };
