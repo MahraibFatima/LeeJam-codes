096d88b9ad740891d4a096bb5a3067765e828d1b
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-19 00:58:48 +0500

    Time: 5 ms (56.83%) | Memory: 7 MB (72.89%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fib(int n) {
+    int cal_fib(int n) {
         if (n==0 || n==1)
             return n;
         else if(n==2)
@@ -8,6 +8,28 @@ public:
         else if(n == 3)
             return 2;
         else
-            return fib(n-1) + fib(n-2);
+            return cal_fib(n-1) + cal_fib(n-2);
+    }
+    int fib(int n) {
+        int res = 0;
+        int arr[n+1];
+        for (int i = 0; i < n+1; ++i) {
+            arr[i] = -1;
+        }
+        if(arr[n] !=-1){
+            res = arr[n];
+        }else{
+            res = cal_fib(n);
+            arr[n] = res;
+        }
+        return res;
+        // if (n==0 || n==1)
+        //     return n;
+        // else if(n==2)
+        //     return 1;
+        // else if(n == 3)
+        //     return 2;
+        // else
+        //     return fib(n-1) + fib(n-2);
     }
 };
