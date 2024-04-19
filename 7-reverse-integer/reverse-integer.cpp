3ed32b12827920ebf2ada65b23860f664d6e8994
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-19 20:57:42 +0500

    Time: 0 ms (100.00%) | Memory: 7 MB (95.11%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -13,4 +13,29 @@ public:
         }
         return reversed;
     }
+
+/*int negative(int n){
+    string s = to_string(n);
+    s = "-" + s;
+    n = stoi(s);
+    return n;
+}
+int reverse(int x) {
+    int temp = x;
+    long int n = 0;
+    bool neg = false;
+    if(x<0){
+        neg = true;
+        x = abs(x);
+    }
+    while(x>0){
+            n = (n*10)+(x%10);
+            x /= 10;
+        }
+    if(neg){
+        n = negative(n);    
+    }
+    return n;
+    }*/
+
 };
