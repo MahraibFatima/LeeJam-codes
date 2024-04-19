492cf015c8514ff01e2ac327f220789280b1f5d3
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-04-19 20:52:35 +0500

    Time: 2 ms (39.15%) | Memory: 7.5 MB (51.93%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long reversed = 0; // Use long long to avoid integer overflow
+        long long reversed = 0;
+
         while (x != 0) {
             reversed = reversed * 10 + x % 10;
             x /= 10;
         }
-        // Check for overflow
-        if (reversed > INT_MAX || reversed < INT_MIN) {
+        
+        if(reversed > INT_MAX || reversed < INT_MIN) {
             return 0;
         }
         return reversed;
     }
-/*int negative(int n){
-    string s = to_string(n);
-    s = "-" + s;
-    n = stoi(s);
-    return n;
-}
-int reverse(int x) {
-    int temp = x;
-    long int n = 0;
-    bool neg = false;
-    if(x<0){
-        neg = true;
-        x = abs(x);
-    }
-    while(x>0){
-            n = (n*10)+(x%10);
-            x /= 10;
-        }
-    if(neg){
-        n = negative(n);    
-    }
-    return n;
-    }*/
 };
