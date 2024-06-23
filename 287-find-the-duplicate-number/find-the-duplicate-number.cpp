aa486491c3920aaa9569fa975c74c4988cb6402f
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-06-23 07:55:28 +0500

    Time: 83 ms (49.19%) | Memory: 63.6 MB (76.60%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,14 +1,31 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         map <int, int> mpp;
+//         for(int i=0; i<nums.size(); i++){
+//             mpp[nums[i]]++;
+//         }
+//         for(const auto& pair : mpp){
+//             if(pair.second >= 2)
+//                 return pair.first;
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map <int, int> mpp;
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
+          int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        for(const auto& pair : mpp){
-            if(pair.second >= 2)
-                return pair.first;
-        }
-        return -1;
+        return slow;
     }
 };
