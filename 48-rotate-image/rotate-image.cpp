27fd274d7c223831f53fc09193cf60e799bb2f01
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-06-26 08:52:51 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (61.08%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+void transpose(vector<vector<int>>& matrix){
+    int rows = matrix.size(), cols = matrix[0].size();
+    for (int i = 0; i < rows; ++i) {
+        for (int j = i + 1; j < cols; ++j) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+void swap_columns(vector<vector<int>>& matrix) {
+    int rows = matrix.size(), cols = matrix[0].size();
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols / 2; j++) { // Only swap half of the columns
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][cols - 1 - j];
+            matrix[i][cols - 1 - j] = temp;
+        }
+    }
+}
+
+
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //int rows = matrix.size(), cols = matrix[0].size();
+        transpose(matrix);
+        swap_columns(matrix);
+
+    }
+};
