4ff9315a22831fe15824e61df55340507fbe3c8f
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-07-03 09:22:54 +0500

    Time: 4 ms (33.14%) | Memory: 8 MB (47.52%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    vector<int> generateRow(int row) {
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1); // inserting the 1st element
+
+        // calculate the rest of the elements:
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> PascalTriangle;
+        for (int i = 1; i <= numRows; ++i) {
+            PascalTriangle.push_back(generateRow(i));
+        }
+        return PascalTriangle;
+    }
+};
