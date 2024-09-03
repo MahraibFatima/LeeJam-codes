4fa42040dd2d5462bae5d39086fc7dca7cf697a0
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-09-04 00:22:18 +0500

    Time: 5 ms (25.74%) | Memory: 10.3 MB (11.28%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int sumOfDigits(string num_str) {
+        int sum = 0;
+        for (char digit : num_str) {
+            sum += digit - '0'; // Convert character to integer and add to sum
+        }
+        return sum;
+    }
+
+public:
+    int getLucky(string s, int k) {
+        string num_str;
+
+        for (char c : s) {
+            int value = c - 'a' + 1;
+            num_str += to_string(value);
+        }
+
+        for (int i = 0; i < k; ++i) {
+            int sum = sumOfDigits(num_str);
+            num_str = to_string(sum);
+        }
+
+        return stoi(num_str);
+    }
+};
