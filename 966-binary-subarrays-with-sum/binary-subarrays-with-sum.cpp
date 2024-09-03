08a53d70fabb65b54476808ddcfb472612e1f7fb
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-09-03 11:03:49 +0500

    Time: 16 ms (89.49%) | Memory: 31.5 MB (38.90%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,38 +1,35 @@
 class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return helper(nums, goal) - helper(nums, goal - 1);
+    }
 
 private:
+    int helper(vector<int>& nums, int goal) {
+        if (goal < 0)
+            return 0;
+
+        int l = 0, r = 0, sum = 0, cnt = 0;
+        while (r < nums.size()) {
+            sum += nums[r++];
+
+            while (sum > goal && l < r)
+                sum -= nums[l++];
+
+            cnt += r - l;
+        }
+        return cnt;
+    }
     // int numSubarraysWithSum(vector<int>& nums, int goal) {
     //     int l = 0, r = 0, sum = 0, cnt = 0;
 
     //     while (r < nums.size()) {
-    //         sum += nums[r];
+    //         sum += nums[r++];
 
-    //         while (sum > goal && l <= r)
+    //         while (sum > goal && l < r)
     //             sum -= nums[l++];
-    //         cnt += r - l + 1;// Count this subarray if it matches the goal
-    //         r++;
+    //         cnt += r - l;// Count this subarray if it matches the goal
     //     }
     //     return cnt;
     // }
-    int helper(vector<int>& nums, int goal) {
-        if (goal < 0)
-            return 0;
-        int l = 0, r = 0, sum = 0, cnt = 0;
-
-        while (r < nums.size()) {
-            sum += nums[r++];
-            while (sum > goal && l < r)
-                sum -= nums[l++];
-
-            //if (sum == goal)
-            cnt += r - l ;
-
-        }
-        return cnt;
-    }
-
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return helper(nums, goal) - helper(nums, goal-1);
-    }
 };
